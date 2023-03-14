8bd25dba9f9866ed363e525678601888b0b0ade4
bondarevy <35042703+bondarevy@users.noreply.github.com>
2023-03-14 22:15:14 +0300

    Create weld.go

---
+++ weld.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "machine"
+    "time"
+)
+
+func main() {
+    // Назначение пинов на выход
+    pins := []machine.Pin{
+        machine.D11,
+        machine.D10,
+        machine.D9,
+        machine.D8,
+        machine.D7,
+        machine.D6,
+        machine.D5,
+        machine.D4,
+    }
+    for _, pin := range pins {
+        pin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+        // Установка пина в LOW
+        pin.Low()
+    }
+    // Задержка в 1 секунду
+    time.Sleep(time.Second)
+}
